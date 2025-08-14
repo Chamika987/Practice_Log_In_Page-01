dee3141be232463d2400b07bdbdb88307bc0a71a
Chamika Shehan <shehanchamika0323@gmail.com>
2025-08-15 01:52:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function signin(){
+    alert("Log In Successfully");
+    document.getElementById("c1").innerHTML = "Loading......";
+}
+
+function signup(){
+    alert("Something is wrong please try again later");
+}
